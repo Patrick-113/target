4bc11a5e40a36d547f88cb5278190aec08773b74
Patrick-113 <rochapatrick113@gmail.com>
2025-07-15 18:06:58 -0300

    Edição da tela de home, com adição dos componentes de Header, Lista e botão de adição de metas.

---
+++ src/theme/index.ts
@@ -0,0 +1,5 @@
+import { colors } from "./colors";
+import { fontFamily } from "./fontFamily";
+
+//Arquivo para facilitar as exportações da pasta theme, deixando em um só lugar
+export { colors, fontFamily}
