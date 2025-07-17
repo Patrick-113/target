bc8c191658d0075782ce6a0e31e40ccd4695bf92
Patrick-113 <rochapatrick113@gmail.com>
2025-07-17 18:30:32 -0300

    Termino da interface visual

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,21 +1,25 @@
 import { PageHeader } from "@/components/PageHeader";
-import { router } from "expo-router";
-import { Button, View } from "react-native";
+import { View } from "react-native";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+import { CurrencyInput } from "@/components/CurrencyInput";
 
 export default function Target(){
   return (
-    <View style={{ flex: 1, padding: 24}}>
-
+    <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
-      title="Meta" 
-      subtitle="Economize para alcançar sua meta financeira."
-      rightButton={{
-        icon: "edit",
-        onPress: () => {}
-      }}
+        title="Meta"
+        subtitle="Economize para alcançar sua meta financeira."
       />
 
-      <Button title="Voltar" onPress={() => router.back()}/>
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input label="Nome da meta"
+        placeholder="Ex: Viagem para praia, Apple Watch"
+        />
+
+        <CurrencyInput label="Valor alvo" value={0}/>
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,15 +1,34 @@
-import { Text, View, Button} from "react-native";
-import { useLocalSearchParams, router} from "expo-router";
+import { View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import { PageHeader } from "@/components/PageHeader";
+import { CurrencyInput } from "@/components/CurrencyInput";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+import { TransactionType } from "@/components/TransactionType";
+import { useState } from "react";
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 export default function Transaction(){
   //params recebe o id da rota, escrito no URL usando o hook useLocalSearchParams
   const params = useLocalSearchParams<{id: string}>();
+  const [type, setType] = useState(TransactionTypes.Input)
 
   return(
-    <View style={{ flex: 1, justifyContent: "center"}}>
-      <Text>ID: {params.id}</Text>
+    <View style={{ flex: 1, padding: 24}}>
+      <PageHeader 
+        title="Nova transação"
+        subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
+      />
 
-      <Button title="Voltar" onPress={() => router.back()}/>
+      <View style={{marginTop: 32, gap: 24}}>
+        <TransactionType selected={type} onChange={setType}/>
+
+        <CurrencyInput label="Valor" value={0}/>
+
+        <Input label="Motivo (opcional)" placeholder="Ex: Investir em CDB de 110% no banco XTPO"/>
+
+        <Button title="Salvar"/>
+      </View>
     </View>
   )
 }
